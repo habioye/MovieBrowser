0f215ba002bd48a0f0e1acd50eef1e8f1745e20d
Hy <hyhuynh222@gmail.com>
2024-07-05 07:36:05 -0700

    Set Up dynamic website for each movie

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OmdbService } from '../omdb.service';
 
 @Component({
   selector: 'app-movie-detail',
@@ -6,9 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './movie-detail.component.css'
 })
 export class MovieDetailComponent {
-
   movie: any;
+  error: any;
   
-  constructor(){}
+  constructor(private route: ActivatedRoute, private movieService: OmdbService){}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    if (id){
+      this.movieService.getMovieById(id).subscribe(data => {
+        this.movie = data;
+        console.log(this.movie)
+        this.error = null;
+      })
+    } else {
+      this.error = '404 No Found'
+      this.movie = null;
+    }
+  }
 
 }
